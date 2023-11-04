3d7d05fe2e862658b1b981ca98d8a6bc8348e1cb
Queendalin Ezenwa <queendalin.nduka@gmail.com>
2023-11-04 23:09:25 +0100

    update product fields and deploy app to internet via fly.io

--- mongo.js
+++ mongo.js
@@ -14,18 +14,41 @@ mongoose.set('strictQuery',false)
 mongoose.connect(url)
 
 const productSchema = new mongoose.Schema({
-  content: String,
-  important: Boolean,
-})
+  amountAvailable: {
+    type: Number,
+    required: true,
+  },
+  cost: {
+    type: Number,
+    required: true,
+  },
+  productName: {
+    type: String,
+    required: true,
+  },
+  sellerId: {
+    type: Number, // Reference to the User model for seller information
+    required: true,
+  },
+});
 
 const Product = mongoose.model('Product', productSchema)
 
 const product = new Product({
-  content: 'Chocolate Milk-Shake',
-  important: true,
+  amountAvailable: 20,
+  cost: 5.99,
+  productName: 'Lemon Cake',
+  sellerId: 1
 })
 
 product.save().then(result => {
   console.log('product saved!')
+  mongoose.connection.close()
+})
+
+Product.find({}).then(result => {
+  result.forEach(product => {
+    console.log(product)
+  })
   mongoose.connection.close()
 })
